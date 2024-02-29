4f519fc86a2127b40b2d32a27a472dd75d73c142
hssSean <144593163+hssSean@users.noreply.github.com>
2024-02-29 14:01:47 +0800

    Update main.c

--- EX#1/main.c
+++ EX#1/main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
-  
-void print_sp(int i, int n){
-    for(int sp = 0; sp < n-1;sp++){
+
+    
+void print_sp(int i, int n) {
+    for(int sp = 0; sp < n-i;sp++){
         printf(" ");
     }
 }
   
-void print_num(int n){
+void print_num(int n) {
     for (int i=0;i<n;i++){
-        printf("%d",n);
+        printf("%d ",n);
     }
     printf("\n");
 }
 
-int main(){
+int main() {
     int rows =6;
     for (int i =0; i<= rows;i++){
         print_sp(i,rows);
         print_num(i);
     }
-}
-
     return 0;
 }
